e30775366a20cf6b93e5c5ff27b77218c838cf5c
GwenDumoulin <gwen.dumoulin@gmail.com>
2024-09-13 16:45:22 +0200

    Add profiles

--- src/index.js
+++ src/index.js
@@ -5,6 +5,7 @@ import Home from './pages/Home'
 import Survey from './pages/Survey'
 import Results from './pages/Results'
 import Freelances from './pages/Freelances'
+import Profile from './pages/Profile'
 import Header from './components/Header'
 import Footer from './components/Footer'
 import Error from './components/Error'
@@ -23,6 +24,7 @@ ReactDOM.render(
             <Route path="/survey/:questionNumber" element={<Survey />} />
             <Route path="/results" element={<Results />} />
             <Route path="/freelances" element={<Freelances />} />
+            <Route path="/profile/:id" element={<Profile />} />
             <Route path="*" element={<Error />} />
           </Routes>
           <Footer />
